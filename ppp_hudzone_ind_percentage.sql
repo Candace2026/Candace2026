f5bf722bb61695b2ddda209190840afde3dcd621
Candace Williams <60080583+Candace2026@users.noreply.github.com>
2022-08-22 21:58:24 -0500

    Update ppp_hudzone_ind_percentage.sql

--- ppp_hudzone_ind_percentage.sql
+++ ppp_hudzone_ind_percentage.sql
@@ -1,7 +1,14 @@
-SELECT
-Hubzone_Indicator, 
+--Using public data provided by the Small Business Association (SBA), we calculate how many HUBZone businesses received PPP loans of >$150K
+--Spaces added between statements readability
+
+SELECT Hubzone_Indicator, 
+--Count the number of loans received by HUBZone businesses
 COUNT(*) AS number_of_loans,
-ROUND(COUNT(*) * 100.0/ SUM(COUNT(*)) OVER(), 2) AS hudzone_percentage
+--Calculate the rounded percentage of the Hubzone loans out of all business loans received
+ROUND(COUNT(*) * 100.0/ SUM(COUNT(*)) OVER(), 2) AS hudzone_percentage     
+
 FROM `cst1-359800.hi_ppp_stdy.ppp_loan_data`
-WHERE Date_Approved BETWEEN DATE("2020-1-1") AND DATE("2020-12-31")
-GROUP BY Hubzone_Indicator;
+
+WHERE Date_Approved BETWEEN DATE("2020-1-1") AND DATE("2020-12-31")    --Filter the approval dates for the year 2020 only
+
+GROUP BY Hubzone_Indicator;         --Isolate the results under Hubzone businesses only
